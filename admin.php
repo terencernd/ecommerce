76cfc9f7c8f2b1a91d095a5b148c565a1abe0af1
Terence randriamandimby <terence.randriamandimbypgmail.com>
2026-02-06 10:25:33 +0100

    update

---
+++ admin.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (!isset($_SESSION['nom']) || $_SESSION['role'] !== 'admin') {
+    header("Location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Espace Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include "menu.php"; ?>
+
+<div class="container mt-5 text-center">
+    <div class="card p-4 shadow">
+        <h2>Bienvenue Admin <?php echo $_SESSION['nom']; ?> !</h2>
+        <p>Gestion du site Volleyball</p>
+
+        <a href="user.php" class="btn btn-primary">Voir espace utilisateur</a>
+        <a href="logout.php" class="btn btn-danger mt-3">Se déconnecter</a>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>
